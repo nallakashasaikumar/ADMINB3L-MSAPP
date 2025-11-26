48e1587bae3304c6b48ba1d49bda3d25d57a3c06
nallakashasaikumar <nallakashasaikumar@gmail.com>
2025-11-26 21:49:30 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,26 @@
+const CACHE_NAME = "admin-app-cache-v1";
+
+const FILES_TO_CACHE = [
+  "/ADMINB3L-MSAPP/",
+  "/ADMINB3L-MSAPP/index.html",
+  "/ADMINB3L-MSAPP/admin.html",
+  "/ADMINB3L-MSAPP/manifest.json",
+  "/ADMINB3L-MSAPP/icons/icon-192.png",
+  "/ADMINB3L-MSAPP/icons/icon-512.png",
+  "/ADMINB3L-MSAPP/style.css",
+  "/ADMINB3L-MSAPP/script.js"
+];
+
+// Install Service Worker
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("b3l-admin-cache").then((cache) => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./icons/icon.png",
-        "./screenshot/screen1.png"
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
 });
 
+// Fetch Cached Files
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
@@ -19,3 +28,4 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+
